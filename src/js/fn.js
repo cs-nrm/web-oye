e341323ec69b423d444e0107f63e19063818e879
carlos <casanchez@nrmweb.com.mx>
2024-04-17 13:23:19 -0600

    preloader

--- src/js/fn.js
+++ src/js/fn.js
@@ -2,7 +2,9 @@ document.addEventListener('astro:before-preparation', ev => {
     console.log('insert spin');
     /*document.querySelector('main').style.opacity = 0.3;*/
     document.querySelector('main').classList.add('loading');
-    document.getElementsByClassName('preloader')[0].style.display('block');
+    /*document.querySelector('.preloader').style.display = 'block';*/
+    document.querySelector('.preloader').classList.add('showpreloader');
+
 });
 
 
@@ -10,5 +12,6 @@ document.addEventListener('astro:page-load', ev => {
     console.log('pageload');
     /*document.querySelector('main').style.opacity = 1;*/
     document.querySelector('main').classList.remove('loading');
-    document.getElementsByClassName('preloader')[0].style.display('none');
+    /*document.querySelector('.preloader').style.display = 'none';*/
+    document.querySelector('.preloader').classList.remove('showpreloader');
 });
