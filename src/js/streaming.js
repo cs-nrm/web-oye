dbc07bb11cf796f4a2ac317971b3b48c314806ef
carlos <casanchez@nrmweb.com.mx>
2024-05-27 10:13:42 -0600

    quitando scroll

--- src/js/streaming.js
+++ src/js/streaming.js
@@ -383,6 +383,20 @@ document.addEventListener('astro:before-preparation', ev => {
 
 document.addEventListener('astro:page-load', ev => {
    // console.log('pageload');
+   /*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId);
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu');
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon');
+    });
+ }
+ 
+ showMenu('nav-toggle','nav-menu');
+
    const getplayingstatus = playerstatus();
     document.querySelector('main').classList.remove('loading');    
     document.querySelector('.preloader').classList.remove('showpreloader');
