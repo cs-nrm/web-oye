568a3cd40778f7a1d830afe2866a78396b834621
Csanchez <casanchez@nrmweb.com.mx>
2026-06-24 11:16:00 -0600

    podcast

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,111 @@
 
 export const SITE_TITLE = 'Oye 89.7 FM';
 export const SITE_DESCRIPTION = 'Oye se Oye';
+
+export const PODCASTS = [
+    {
+        nodo: 13766,
+        title: "Reset con Kinky",
+        link: "reset-con-kinky",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/portada_podcast_kinky.jpg",
+        descripcion: "Reset con Kinky.",
+    },
+    {
+        nodo: 9046,
+        title: "El camino de las estrellas con Jaidy Robles",
+        link: "el-camino-de-las-estrellas",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_elcamino-min.jpg",
+        descripcion: "¿Te gustaría saber, cómo iniciaron tus artistas favoritos? Jaidy Robles te dirá detalle a detalle de tu artista y mucho más.",
+    },
+    {
+        nodo: 9047,
+        title: "Aquí y Ahora platicamos con Sergio Mejía",
+        link: "aqui-y-ahora",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_aqui-min.jpg",
+        descripcion: "Lo importante de la vida es vivir en el Aquí y en el Ahora. Expertos llegan con Sergio Mejía para contarnos sobre sus experiencias y todo lo que saben.",
+    },
+    {
+        nodo: 9048,
+        title: "Noticias que parecen broma pero no son con Aura Ramírez",
+        link: "noticias-que-parecen-broma-pero-no-son",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_noticias-min.jpg",
+        descripcion: "Ya llegóoooo el que será tu próximo podcast favorito. Soy Aura Ramírez y cada semana te traeré un combo de noticias que parecen falsas pero en realidad NO lo son.",
+    },
+    {
+        nodo: 9049,
+        title: "Lo más viral con Emilio Catalán",
+        link: "lo-mas-viral",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_viral2-min-min.jpg",
+        descripcion: "Los trends de la semana y temas virales en redes sociales.",
+    },
+    {
+        nodo: 9050,
+        title: "Tips y recomendaciones con Sharon Azoulay",
+        link: "tips-y-recomendaciones",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_tips-min.jpg",
+        descripcion: "Los mejores libros para leer y la belleza en un solo podcast.",
+    },
+    {
+        nodo: 9051,
+        title: "Receta fit con la pelirroja de la radio",
+        link: "receta-fit",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_recetafit-min.jpg",
+        descripcion: "Las mejores recetas fitness para tu día a día de forma fácil y rápida.",
+    },
+    {
+        nodo: 9052,
+        title: "Alguien lo tiene que hacer con Poncho Yezka",
+        link: "alguien-lo-tiene-que-hacer",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_alguien-min.jpg",
+        descripcion: "",
+    },
+    {
+        nodo: 3813,
+        title: "Top Tech con Geekzilla",
+        link: "top-tech",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/TOP-TECH-PODCAST.jpg",
+        descripcion: "Top Tech con Geekzilla. un podcast en el que conoceremos todo sobre la tecnología que nos está rodeando.",
+    },
+    {
+        nodo: 3801,
+        title: "Top Gaming con Geekzilla",
+        link: "top-gaming",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/TOP-GAMING-450.jpg",
+        descripcion: "Un clavado al mundo de los gamers en el cual conocerás todo sobre los lanzamientos, mejores juegos del momento y los que vienen.",
+    },
+    {
+        nodo: 6581,
+        title: "Top Ten",
+        link: "top-ten",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/TOP-TEN-450.jpg",
+        descripcion: "La música más top, nueva y relevante que escuchas, tu votas por tener tu canción en los 10 primeros lugares.",
+    },
+    {
+        nodo: 7404,
+        title: "Oye la alternativa",
+        link: "oye-la-alternativa",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_alternativa.png",
+        descripcion: "El espacio donde toda la música se escucha, sin distinción, entrevistas de todos los géneros en un solo lugar.",
+    },
+    {
+        nodo: 6806,
+        title: "Al otro lado de la radio",
+        link: "al-otro-lado-de-la-radio",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/PODCAST_otro-lado.jpg",
+        descripcion: "Un podcast donde podrás escuchar a tus artistas favoritos y conocer más sobre ellos.",
+    },
+    {
+        nodo: 9138,
+        title: "Oye de Cine",
+        link: "oye-de-cine",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/podcast/OYE-CINE-450.jpg",
+        descripcion: "",
+    },
+    {
+        nodo: 10689,
+        title: "La Regadera",
+        link: "la-regadera",
+        imagen: "https://storage.googleapis.com/nrm-web/oye/recursos/PODCAST-OYE.png",
+        descripcion: "",
+    },
+];
